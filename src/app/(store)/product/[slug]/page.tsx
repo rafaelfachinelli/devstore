bfde194a0d48128db13a8c792fab35c7360f211b
rafaelfachinelli <rafael.fachinelli@hotmail.com>
2024-02-24 17:13:17 -0300

    feat: Add new components and update metadata

--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -2,6 +2,8 @@ import Image from 'next/image'
 
 import { api } from '@/data/api'
 import { Product } from '@/data/types/product'
+import { Metadata } from 'next'
+import { AddToCartButton } from '@/components/add-to-cart-button'
 
 interface ProductPageProps {
   params: {
@@ -18,6 +20,29 @@ async function getProduct(slug: string): Promise<Product> {
 
   const product = await response.json()
   return product
+}
+
+export async function generateMetadata({
+  params,
+}: ProductPageProps): Promise<Metadata> {
+  const product = await getProduct(params.slug)
+
+  return {
+    title: product.title,
+  }
+}
+
+export async function generateStaticParams() {
+  const response = await api('/products/featured', {
+    next: {
+      revalidate: 60 * 60, // 1 hour
+    },
+  })
+  const products: Product[] = await response.json()
+
+  return products.map((product) => ({
+    slug: product.slug,
+  }))
 }
 
 export default async function ProductPage({ params }: ProductPageProps) {
@@ -91,12 +116,7 @@ export default async function ProductPage({ params }: ProductPageProps) {
           </div>
         </div>
 
-        <button
-          type="button"
-          className="mt-8 flex h-12 items-center justify-center rounded-full bg-emerald-600 font-semibold text-white"
-        >
-          Adicionar ao carrinho
-        </button>
+        <AddToCartButton productId={product.id} />
       </div>
     </div>
   )
